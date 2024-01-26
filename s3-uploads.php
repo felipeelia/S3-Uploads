364057a09b4254e1baae7f5a44ec2eb18713d861
Felipe Elia <felipe.elia@get10up.com>
2024-01-26 10:57:15 -0300

    Change autoload method

--- s3-uploads.php
+++ s3-uploads.php
@@ -9,6 +9,23 @@ Author URI: https://hmn.md
 */
 
 require_once __DIR__ . '/vendor-prefixed/autoload.php';
+
+spl_autoload_register(
+	function( $classname ) {
+		// Remove the namespace
+		$classname = preg_replace( '/^S3_Uploads\\\/', '', $classname );
+
+		// Format it
+		$class = str_replace( [ '_', '\\' ], [ '-', DIRECTORY_SEPARATOR ], strtolower( $classname ) );
+
+		$classpath = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . 'class-' . $class . '.php';
+
+		if ( file_exists( $classpath ) ) {
+			include_once $classpath;
+		}
+	}
+);
+
 require_once __DIR__ . '/inc/namespace.php';
 
 add_action( 'plugins_loaded', 'S3_Uploads\\init', 0 );
